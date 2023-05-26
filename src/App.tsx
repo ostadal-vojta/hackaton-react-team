057b8b0200a914ed5584040a1801c51c8cbed859
ostadal-vojta <vojtech.ostadal@3pillarglobal.com>
2023-05-26 14:32:16 +0200

    colors

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,16 @@ import Kanban from "./features/kanban/Kanban";
 import Login from "./features/login/Login";
 import Settings from './features/settings/Settings';
 import ProtectedRoute from './features/layout/ProtectedRoute';
-
+import { useEffect } from "react";
 
 function App() {
+
+  useEffect(() => {
+    document.documentElement.style.setProperty('--primary-color', localStorage.getItem('--primary-color') ?? '#249deb')
+    document.documentElement.style.setProperty('--secondary-color', localStorage.getItem('--secondary-color') ?? '#4ac7c4')
+    document.documentElement.style.setProperty('--ternary-color', localStorage.getItem('--ternary-color') ?? '#824aea')
+    document.documentElement.style.setProperty('--text-color', localStorage.getItem('--text-color') ?? '#1e3a8a')
+  }, []);
 
   return (
     <>
