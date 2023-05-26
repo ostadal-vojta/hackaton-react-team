08ef64b75b3e1634966b9f136712d107e65083e7
ostadal-vojta <vojtech.ostadal@3pillarglobal.com>
2023-05-26 12:45:09 +0200

    idk

---
+++ src/features/layout/Layout.tsx
@@ -0,0 +1,40 @@
+import { Routes, Route } from "react-router-dom";
+import Kanban from "../kanban/Kanban";
+import Login from "../login/login";
+
+
+const Laoyout = () => {
+    return (
+        <>
+            <header className="the-header">
+                <a href="index.html" className="the-header__logo">
+                    <span aria-hidden="true" className="the-header__logo-emblem">3C</span>
+                    <span>3 Pillar <strong>Canban</strong></span>
+                </a>
+                <nav className="the-nav">
+                    <ul>
+                        <li>
+                            <a className="the-nav__item" href="index.html">Canban</a>
+                        </li>
+                        <li>
+                            <a className="the-nav__item the-nav__item--active" href="settings.html">Settings</a>
+                        </li>
+                        <li>
+                            <a className="the-nav__item the-nav__item--logout" href="login.html">Log Out</a>
+                        </li>
+                    </ul>
+                </nav>
+
+            </header>
+            <Routes>
+                <Route index element={<Kanban />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/settings" element={<></>} />
+            </Routes>
+        </>
+
+    );
+}
+
+
+export default Laoyout;
